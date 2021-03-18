2ad1709aed6bbf5e8575c9ae0bdb72ef0edf2021
Taewook Kim <Taewook Kim@Taewook-LGgram>
2021-03-18 15:44:41 +0900

    형변환

---
+++ src/Taewook/basic/day03/TestCast.java
@@ -0,0 +1,11 @@
+package Taewook.basic.day03;
+
+public class TestCast {
+	public static void main(String[] args) {
+		byte x = 1, y = 2, z = (byte) (x + y);
+		double avg = (x + y) / 2;
+		System.out.println("평균은" + avg);
+
+		System.out.println("//hello" + '9' + 7);
+	}
+}
